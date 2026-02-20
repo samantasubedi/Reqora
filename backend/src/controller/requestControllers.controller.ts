ac319f7d80cfb89264324c1ad8736c98aa55a46f
samantasubedi <samantasubedi01@gmail.com>
2026-02-20 19:54:42 +0545

    creating request forward endpoint

--- backend/src/controller/requestControllers.controller.ts
+++ backend/src/controller/requestControllers.controller.ts
@@ -1,23 +1,29 @@
-import { Request,Response } from "express";
-export const getAllRequest = (req:Request, res:Response) => {
+import { Request, Response } from "express";
+export const getAllRequest = (req: Request, res: Response) => {
   res.send("gets all requests used to be viewed by manager");
-}
-export const getSpecificRequest= (req:Request, res:Response) => {
+};
+export const getSpecificRequest = (req: Request, res: Response) => {
   const id = req.params.id;
   res.send(`gets a specific request with id ${id}`);
-}
-export const createRequest=(req:Request, res:Response) => {
+};
+export const createRequest = (req: Request, res: Response) => {
   res.json({ message: "used to create a request" });
-}
-export const handleApprove =(req:Request, res:Response) => {
+};
+export const handleApprove = (req: Request, res: Response) => {
   const id = req.params.id;
   res.json({ message: `approves the specific request with id ${id}` });
-}
-export const handleReject = (req:Request, res:Response) => {
+};
+export const handleReject = (req: Request, res: Response) => {
   const id = req.params.id;
   res.json({ message: `rejects the specific request with id ${id}` });
-}
-export const handleCancel= (req:Request, res:Response) => {
+};
+export const handleCancel = (req: Request, res: Response) => {
   const id = req.params.id;
   res.json({ message: `cancels the specific request with id ${id}` });
-}
+};
+export const handleForward = (req: Request, res: Response) => {
+  const id = req.params.id;
+  res.json({
+    message: `forwards the specific request with id ${id} to higher authority`,
+  });
+};
--- backend/src/routes/requestRoutes.route.ts
+++ backend/src/routes/requestRoutes.route.ts
@@ -6,6 +6,7 @@ import {
   handleApprove,
   handleCancel,
   handleReject,
+  handleForward,
 } from "../controller/requestControllers.controller";
 const router = Router();
 router.get("/requests", getAllRequest);
@@ -13,5 +14,6 @@ router.get("/requests/:id", getSpecificRequest);
 router.post("/requests", createRequest);
 router.post("/requests/:id/approve", handleApprove);
 router.post("/requests/:id/reject", handleReject);
+router.post("/requests/:id/forward", handleForward);
 router.post("/requests/:id/cancel", handleCancel);
 export default router;
