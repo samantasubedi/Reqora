485c99c91ecaa691d11ff6ed02d95078cf31b064
samantasubedi <samantasubedi01@gmail.com>
2026-02-21 20:52:42 +0545

    making user pages

---
+++ frontend/app/employeePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const employeePage = () => {
+  return <div>employeePage</div>;
+};
+
+export default employeePage;
---
+++ frontend/app/managerPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const managerPage = () => {
+  return (
+    <div>managerPage</div>
+  )
+}
+
+export default managerPage
