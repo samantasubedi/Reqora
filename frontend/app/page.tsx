f0f5c82dad0b7eb4e9d5275523e45a567829d8c7
samantasubedi <samantasubedi01@gmail.com>
2026-03-24 23:25:38 +0545

    fixing request mechanism

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,33 +4,37 @@ import Test from "@/components/Test";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { EffectCallback, useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
-  const isLoggedIn = async () => {
-    try {
-      const response = await axios.post(`${backendUrl}/isloggedin`, null, {
-        withCredentials: true,
-      });
 
-      if (response.data.code == "LOGGEDIN") {
-        const { role, username } = response.data;
-        router.push(`/${role}/dashboard`);
-        return;
-      } else if (response.data.code == "NOT_LOGGEDIN") {
-        console.log(response.data.message);
-        return;
-      } else if (response.data.code == "TOKEN_REFRESHED") {
-        router.push("/");
-        console.log(response.data.message);
-        return;
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      try {
+        const response = await axios.post(`${backendUrl}/isloggedin`, null, {
+          withCredentials: true,
+        });
+
+        if (response.data.code == "LOGGEDIN") {
+          const { role, username } = response.data;
+          router.push(`/${role}/dashboard`);
+          return;
+        } else if (response.data.code == "NOT_LOGGEDIN") {
+          console.log(response.data.message);
+          return;
+        } else if (response.data.code == "TOKEN_REFRESHED") {
+          router.push("/");
+          console.log(response.data.message);
+          return;
+        }
+      } catch (err) {
+        console.log("request failed", err);
       }
-    } catch (err) {
-      console.log("request failed", err);
-    }
-  };
-  isLoggedIn();
+    };
+    isLoggedIn();
+  }, []);
   return (
     <>
       {" "}
