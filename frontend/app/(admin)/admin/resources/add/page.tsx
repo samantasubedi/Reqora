89cb004c36694c40144be7a65e8bba250c65b3e6
samantasubedi <samantasubedi01@gmail.com>
2026-04-24 23:28:02 +0545

    handling form submission

--- frontend/app/(admin)/admin/resources/add/page.tsx
+++ frontend/app/(admin)/admin/resources/add/page.tsx
@@ -11,6 +11,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import z from "zod";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { T_MutaionError } from "@/types/global";
 const schema = z.object({
   resourceName: z
     .string({ message: "please enter the resource name" })
@@ -26,6 +30,10 @@ const schema = z.object({
   status: z
     .string({ message: "please select a status" })
     .min(1, "please select a status"),
+  location: z
+    .string({ message: "please enter a location" })
+    .min(1,"Please enter a location")
+    .min(3, "please enter a valid location"),
   department: z
     .string({ message: "please enter the department" })
     .min(1, "please enter a department"),
@@ -38,12 +46,37 @@ const page = () => {
     setError,
     formState: { errors },
     handleSubmit,
+    reset,
   } = useForm({ resolver: zodResolver(schema) });
+
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+  const postApi = async (data: formDataType) => {
+    const response = await axios.post(`${backendUrl}/resources`, data, {
+      withCredentials: true,
+    });
+    return response.data;
+  };
+  const mutation = useMutation({
+    mutationFn: postApi,
+    onSuccess: (data) => {
+      toast.success(data.message);
+      reset();
+    },
+    onError: (error: T_MutaionError) => {
+      if(error.response){  toast.error(error.response?.data.message)}
+      else{
+        toast.error(error.message)
+      }
+    
+    },
+  });
+
   const formSubmitHandler = (data: formDataType) => {
+    mutation.mutate(data);
     console.log(data);
   };
   return (
-    <div className="flex justify-center items-start min-h-screen bg-slate-50 py-12 px-4">
+    <div className="flex justify-center items-start min-h-screen bg-sky-200 py-12 px-4">
       <Card className="w-full max-w-2xl shadow-xl border-0 rounded-2xl overflow-hidden bg-blue-100">
         <div className="h-1.5 bg-linear-to-r from-violet-500 via-indigo-500 to-blue-500" />
         <CardHeader className="text-center px-10 pt-10 pb-6">
@@ -132,6 +165,19 @@ const page = () => {
                   {errors.department?.message}
                 </p>
               </div>
+            </div>
+                <div className="space-y-1.5">
+              <label className="text-sm font-semibold text-slate-700 tracking-wide uppercase">
+                Location
+              </label>
+              <Input
+                {...register("location")}
+                placeholder="Enter the resource location"
+                className="h-11  rounded-lg border-slate-200 bg-white focus:ring-2 focus:ring-violet-400 focus:border-transparent transition"
+              />
+              <p className="text-xs text-red-500 font-medium">
+                {errors.location?.message}
+              </p>
             </div>
 
             <div className="space-y-1.5">
--- frontend/types/global.ts
+++ frontend/types/global.ts
@@ -4,7 +4,6 @@ type T_BackendRespose = {
   message: string;
   code: string;
   success: boolean;
-  error: {};
 };
 
 export type T_MutaionError = AxiosError<T_BackendRespose>;
