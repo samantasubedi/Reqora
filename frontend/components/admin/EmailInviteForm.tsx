55498943ac5391ca40c0a332c3be45bb1e7bbe01
samantasubedi <samantasubedi01@gmail.com>
2026-04-26 23:52:00 +0545

    adding validation in email form

--- frontend/components/admin/EmailInviteForm.tsx
+++ frontend/components/admin/EmailInviteForm.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import z, { email } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const schema = z.object({
+  email: z.email("Please enter an email").min(1, "Please enter an email"),
+  role: z.string("Please select a role").min(1, "Please select a role"),
+  description: z.string().optional(),
+});
+type formType = z.infer<typeof schema>;
 
 const EmailInviteForm = () => {
   const roleArray = [
@@ -18,64 +27,87 @@ const EmailInviteForm = () => {
       description: "Full workspace access",
     },
   ];
+  const {
+    register,
+    formState: { errors },
+    setError,
+    setValue,
+    handleSubmit,
+  } = useForm({ resolver: zodResolver(schema) });
+  const handleFormSubmit = (data: formType) => {
+    console.log(data);
+  };
   return (
     <Card className="w-[40%] mx-auto shadow-lg border bg-blue-100 rounded-2xl mt-5">
-      <CardContent className="p-6">
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-2">
-            <label className="text-md font-medium text-slate-700">
-              Email Address
-            </label>
-            <Input
-              placeholder="Enter email"
-              className="focus:ring-2 focus:ring-blue-500 bg-white"
-            />
-          </div>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <CardContent className="p-6">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <label className="text-md font-medium text-slate-700">
+                Email Address
+              </label>
+              <Input
+                {...register("email")}
+                placeholder="Enter email"
+                className="focus:ring-2 focus:ring-blue-500 bg-white"
+              />
+              <p className="text-sm text-red-500">{errors.email?.message}</p>
+            </div>
 
-          <div className="flex flex-col gap-2">
-            <label className="text-md font-medium text-slate-700">Role</label>
-            <div className="flex gap-5">
-              {roleArray.map((curr) => {
-                return (
-                  <button
-                  key={curr.role}
-                    className="h-20 flex flex-1 justify-center items-center  px-4 py-2 rounded-lg border border-slate-300 
+            <div className="flex flex-col gap-2">
+              <label className="text-md font-medium text-slate-700">Role</label>
+              <input type="hidden" {...register("role")}></input>
+              <div className="flex gap-5">
+                {roleArray.map((curr) => {
+                  return (
+                    <button
+                      key={curr.role}
+                      className="h-20 flex flex-1 justify-center items-center  px-4 py-2 rounded-lg border border-slate-300 
               hover:bg-slate-100 hover:border-slate-400  bg-white
               transition-all duration-200"
-                  >
-                    <div>
-                      <div className="font-semibold text-lg text-slate-600 font-sans">
-                        {curr.role}
+                      onClick={() => {
+                        setValue("role", curr.role);
+                      }}
+                    >
+                      <div>
+                        <div className="font-semibold text-lg text-slate-600 font-sans">
+                          {curr.role}
+                        </div>
+                        <div className="text-sm text-slate-500">
+                          {curr.description}
+                        </div>
                       </div>
-                      <div className="text-sm text-slate-500">
-                        {curr.description}
-                      </div>
-                    </div>
-                  </button>
-                );
-              })}
+                    </button>
+                  );
+                })}
+              </div>
+              <p className="text-sm text-red-500">{errors.role?.message}</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label className="text-md font-medium text-slate-700">
+                Message
+              </label>
+              <textarea
+                {...register("description")}
+                rows={3}
+                placeholder="Write a message (optional)"
+                className="border border-slate-300 rounded-lg p-3 text-sm 
+          focus:outline-none  bg-white"
+              />
             </div>
           </div>
+        </CardContent>
 
-          <div className="flex flex-col gap-2">
-            <label className="text-md font-medium text-slate-700">
-              Message
-            </label>
-            <textarea
-              rows={3}
-              placeholder="Write a message (optional)"
-              className="border border-slate-300 rounded-lg p-3 text-sm 
-          focus:outline-none  bg-white"
-            />
-          </div>
-        </div>
-      </CardContent>
-
-      <CardFooter className="p-6 pt-0">
-        <Button className="w-full rounded-lg text-sm font-medium shadow-sm hover:shadow-md transition-all bg-blue-500 cursor-pointer hover:bg-blue-400">
-          Send Invitation
-        </Button>
-      </CardFooter>
+        <CardFooter className="p-6 pt-0">
+          <Button
+            type="submit"
+            className="w-full rounded-lg text-sm font-medium shadow-sm hover:shadow-md transition-all bg-blue-500 cursor-pointer hover:bg-blue-400"
+          >
+            Send Invitation
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 };
