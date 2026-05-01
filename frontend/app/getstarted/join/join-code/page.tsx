9e6d371941f552c5d3b2f225ab1100eec2a8bde9
samantasubedi <samantasubedi01@gmail.com>
2026-05-01 16:40:32 +0545

    creating join code form

--- frontend/app/getstarted/join/join-code/page.tsx
+++ frontend/app/getstarted/join/join-code/page.tsx
@@ -1,7 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Icon } from "@iconify/react";
 import React from "react";
 
 const page = () => {
-  return <div>this is join through code page</div>;
+  return (
+    <div className="flex justify-center">
+      <Card className="md:w-[30%] md:mt-[10%]">
+        <CardHeader>
+          <CardTitle>  <Icon icon="mdi:people" width="24" height="24" />Join a Company</CardTitle>
+          <CardDescription>
+            Enter the company code shared by your administrator.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <label> Join Code</label>
+          <Input placeholder="eg: H3E0klMT3f"></Input>
+        </CardContent>
+        <CardFooter>
+          <Button>Join</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default page;
