026139804cd28c1366e1f927e2cc98a96d5b9258
samantasubedi <samantasubedi01@gmail.com>
2026-05-05 21:02:09 +0545

    handling code input

--- frontend/app/getstarted/join/join-code/page.tsx
+++ frontend/app/getstarted/join/join-code/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,7 +13,7 @@ import { T_MutaionError } from "@/types/global";
 import { Icon } from "@iconify/react";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 
 const page = () => {
@@ -38,16 +39,16 @@ const page = () => {
       }
     },
   });
+  const [joinCode, setJoinCode] = useState("");
   return (
     <div className="flex justify-center">
       <Card className="md:w-[30%] md:mt-[10%] bg-white">
         <CardHeader className="p-4 bg-teal-100 rounded-b-2xl">
           <CardTitle className="flex gap-5 text-2xl justify-center items-center">
-            {" "}
             <Icon
               icon="mdi:people"
               className="text-5xl! bg-white rounded-full p-2 text-teal-800"
-            />{" "}
+            />
             <span className="text-teal-800">Join a Company</span>
           </CardTitle>
           <CardDescription className="text-gray-600 font-semibold text-center">
@@ -57,16 +58,23 @@ const page = () => {
 
         <CardContent>
           <label className="text-xl font-semibold text-teal-700">
-            {" "}
             Join Code
           </label>
           <Input
+            onChange={(e) => {
+              setJoinCode(e.target.value);
+            }}
             placeholder="eg: H3E0klMT3f"
             className="bg-white mt-2 h-14! text-2xl! font-semibold text-teal-800 border-teal-500"
           ></Input>
         </CardContent>
         <CardFooter>
-          <Button className="bg-teal-700 w-full! font-extrabold text-lg mt-5 hover:bg-teal-600 cursor-pointer">
+          <Button
+            onClick={() => {
+              console.log(joinCode);
+            }}
+            className="bg-teal-700 w-full! font-extrabold text-lg mt-5 hover:bg-teal-600 cursor-pointer"
+          >
             Join
           </Button>
         </CardFooter>
