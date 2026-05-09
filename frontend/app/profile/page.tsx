120acd79cb91c64b826b50f3a28c6b58b925e9db
samantasubedi <samantasubedi01@gmail.com>
2026-05-09 23:41:47 +0545

    designing profile page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,7 +1,26 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Icon } from "@iconify/react";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  const username = "samanta";
+  const email = "samantasubedi01@gmail.com";
+  const role = "admin";
+  const companyName = "comapny1";
+  return (
+    <div>
+      <Card>
+        <CardContent>
+          <Icon icon="material-symbols:person-rounded" width="24" height="24" />
+          <div>
+            <p>{username}</p>
+            <p>{email}</p>
+            <p>{role}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default page;
