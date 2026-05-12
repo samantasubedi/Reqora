10e4454d572075ee1b82650538905423feef7530
samantasubedi <samantasubedi01@gmail.com>
2026-05-12 22:22:09 +0545

    fetching profile data from backend

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Icon } from "@iconify/react";
+import {
+  QueryClientProvider,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 const page = () => {
@@ -8,17 +14,35 @@ const page = () => {
   const email = "samantasubedi01@gmail.com";
   const role = "admin";
   const companyName = "comapny1";
+  const queryClient = useQueryClient();
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+  const fetchProfileInfo = async () => {
+    const response = await axios.get(`${backendUrl}/profile`);
+    return response.data;
+  };
+  const query = useQuery({
+    queryFn: fetchProfileInfo,
+    queryKey: ["retrivedProfileInfo"],
+  });
   return (
     <div className="flex justify-center p-5">
       <Card className="w-[60%] p-10">
-        <CardContent className="flex justify-between" >
+        <CardContent className="flex justify-between">
           <div className="flex gap-4">
-         <div><Icon icon="material-symbols:person-rounded" className="text-7xl p-2 rounded-full bg-gray-200" /></div> 
-          <div>
-            <p className="font-bold text-2xl">{username}</p>
-            <p className="text-gray-600">{email}</p>
-            <p className="text-blue-600 px-2 rounded-2xl bg-blue-200 w-fit font-semibold">{role}</p>
-          </div></div>
+            <div>
+              <Icon
+                icon="material-symbols:person-rounded"
+                className="text-7xl p-2 rounded-full bg-gray-200"
+              />
+            </div>
+            <div>
+              <p className="font-bold text-2xl">{username}</p>
+              <p className="text-gray-600">{email}</p>
+              <p className="text-blue-600 px-2 rounded-2xl bg-blue-200 w-fit font-semibold">
+                {role}
+              </p>
+            </div>
+          </div>
           <Button>Edit profile</Button>
         </CardContent>
       </Card>
