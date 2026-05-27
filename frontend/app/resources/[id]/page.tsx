80a8a7fe9c8e11857b2f6ab3fb37231d4dc0bbc3
samantasubedi <samantasubedi01@gmail.com>
2026-05-27 16:46:18 +0545

    writting fetch api

--- frontend/app/resources/[id]/page.tsx
+++ frontend/app/resources/[id]/page.tsx
@@ -1,10 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 type propType = {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 };
 const ResourceDetails = async ({ params }: propType) => {
-  return <div>{`this is id ${params.id}`}</div>;
+  console.log(params);
+  const resolvedParams = await params;
+  const id = resolvedParams.id;
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+  const fetchApi = async () => {
+    const response = await axios.post(`${backendUrl}/resource/${id}`);
+    return response.data;
+  };
+  const query = useQuery({
+    queryKey: ["resourceDetail"],
+    queryFn: fetchApi,
+  });
+
+  return <div>this is resource details page</div>;
 };
 
 export default ResourceDetails;
