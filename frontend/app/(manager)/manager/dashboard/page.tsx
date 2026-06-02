6f415af8effbfb18adb6b0e347f5fdda14767047
samantasubedi <samantasubedi01@gmail.com>
2026-06-02 22:35:46 +0545

    making resource table component

--- frontend/app/(manager)/manager/dashboard/page.tsx
+++ frontend/app/(manager)/manager/dashboard/page.tsx
@@ -19,63 +19,7 @@ const page = () => {
       >
         Logout
       </Button>
-      <Table>
 
-  <TableHeader>
-
-    <TableRow>
-      <TableHead>Employee</TableHead>
-      <TableHead>Resource</TableHead>
-      <TableHead>Quantity</TableHead>
-      <TableHead>Date</TableHead>
-      <TableHead>Status</TableHead>
-      <TableHead>Action</TableHead>
-    </TableRow>
-
-  </TableHeader>
-
-  <TableBody>
-
-    <TableRow>
-
-      <TableCell>John</TableCell>
-
-      <TableCell>Laptop</TableCell>
-
-      <TableCell>2</TableCell>
-
-      <TableCell>2 Jun 2026</TableCell>
-
-      <TableCell>
-        <Badge>
-          Pending
-        </Badge>
-      </TableCell>
-
-      <TableCell>
-
-        <div className="flex gap-2">
-
-          <Button size="sm">
-            Approve
-          </Button>
-
-          <Button
-            size="sm"
-            variant="destructive"
-          >
-            Reject
-          </Button>
-
-        </div>
-
-      </TableCell>
-
-    </TableRow>
-
-  </TableBody>
-
-</Table>
     </>
   );
 };
---
+++ frontend/components/manager/RequestTable.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Badge } from '../ui/badge'
+import { Button } from '../ui/button'
+
+export const RequestTable = () => {
+  return (
+    <div>
+              <Table>
+
+  <TableHeader>
+
+    <TableRow>
+      <TableHead>Employee</TableHead>
+      <TableHead>Resource</TableHead>
+      <TableHead>Quantity</TableHead>
+      <TableHead>Date</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Action</TableHead>
+    </TableRow>
+
+  </TableHeader>
+
+  <TableBody>
+
+    <TableRow>
+
+      <TableCell>John</TableCell>
+
+      <TableCell>Laptop</TableCell>
+
+      <TableCell>2</TableCell>
+
+      <TableCell>2 Jun 2026</TableCell>
+
+      <TableCell>
+        <Badge>
+          Pending
+        </Badge>
+      </TableCell>
+
+      <TableCell>
+
+        <div className="flex gap-2">
+
+          <Button size="sm">
+            Approve
+          </Button>
+
+          <Button
+            size="sm"
+            variant="destructive"
+          >
+            Reject
+          </Button>
+
+        </div>
+
+      </TableCell>
+
+    </TableRow>
+
+  </TableBody>
+
+</Table>
+    </div>
+  )
+}
