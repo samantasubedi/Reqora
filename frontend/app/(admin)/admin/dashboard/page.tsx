413e6367ae25a86fe35f3e225a4de3b7df51f179
samantasubedi <samantasubedi01@gmail.com>
2026-08-27 10:30:00 +0545

    refactoring the sidebar behaviour

--- frontend/app/(admin)/admin/dashboard/page.tsx
+++ frontend/app/(admin)/admin/dashboard/page.tsx
@@ -1,10 +1,15 @@
 import { AdminDashboard } from "@/components/admin/AdminDashboard";
 import SidebarMenu from "@/components/admin/Sidebarmenu";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Icon } from "@iconify/react";
 
 export default function AdminDashboardPage() {
   return (
     <div className="flex max-w-screen">
       <SidebarMenu />
+       <SidebarTrigger>
+                <Icon icon="charm:menu-hamburger"></Icon>
+              </SidebarTrigger>
       <div className="w-screen">
         <AdminDashboard />
       </div>
--- frontend/components/admin/Sidebarmenu.tsx
+++ frontend/components/admin/Sidebarmenu.tsx
@@ -12,6 +12,7 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenuButton,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import {
   DropdownMenu,
@@ -29,185 +30,198 @@ import LogoutDialog from "./LogoutDialog";
 
 const Sidebarmenu = () => {
   const router = useRouter();
+  const { state } = useSidebar();
   return (
-    <Sidebar>
-      
-      <SidebarHeader className="bg-sidebar">
-        <button
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          <Image
-            src="/reqoraLogo.png"
-            width={500}
-            height={500}
-            alt="logo"
-            className="w-50 h-25"
-          ></Image>
-        </button>
-      </SidebarHeader>
-      <SidebarContent className="bg-sidebar text-text-primary">
-        <SidebarGroup>
-          <SidebarGroupLabel className=" font-bold">
-            Main
-          </SidebarGroupLabel>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton
-                asChild
-                suppressHydrationWarning
-                className="flex justify-start"
-              >
-                <Button
-                  className="bg-transparent font-bold"
-                  variant={"secondary"}
-                >
-                  <Icon
-                    icon="material-symbols-light:home-rounded"
-                    className="size-6!"
-                  />
-                  Dashboard
-                </Button>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroup>
-
-        <SidebarGroup>
-          <SidebarGroupLabel className="  font-bold">
-            Management
-          </SidebarGroupLabel>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild suppressHydrationWarning>
-                  <SidebarMenuButton
-                    suppressHydrationWarning
-                    className="flex justify-start  font-bold"
-                  >
-                    <Icon icon="grommet-icons:resources" className="size-5!" />
-                    Resources
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem
-                    onClick={() => {
-                      router.push("/resources");
-                    }}
-                  >
-                    <Icon icon="material-symbols:grid-view-outline-rounded" />{" "}
-                    View Resources
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    onClick={() => {
-                      router.push("/admin/resources/add");
-                    }}
-                  >
-                    <Icon icon="hugeicons:resources-add" />
-                    Add Resources
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild suppressHydrationWarning>
-                  <SidebarMenuButton
-                    suppressHydrationWarning
-                    className="  font-bold"
-                  >
-                    <Icon icon="ic:baseline-people" className="size-5! " />
-                    Employees
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem
-                    onClick={() => {
-                      router.push("/admin/employees");
-                    }}
-                  >
-                    <Icon icon="material-symbols:view-day-rounded" /> View
-                    Employees
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    onClick={() => {
-                      router.push("/admin/invite");
-                    }}
-                  >
-                    <Icon icon="mdi:invite" />
-                    Invite Employees
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Icon icon="carbon:user-role" /> Manage Roles
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Icon icon="mingcute:user-remove-fill" />
-                    Remove Employees
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroup>
-        <SidebarGroup>
-          <SidebarGroupLabel className="  font-bold">
-            System
-          </SidebarGroupLabel>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton
-                suppressHydrationWarning
-                className="  font-bold"
-              >
-                <Icon icon="material-symbols:settings" className="size-5!" />
-                Settings
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <SidebarMenuButton
-                suppressHydrationWarning
-                className="  font-bold"
-              >
-                <Icon icon="tabler:logs" className="size-5!" />
-                <span>Logs</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter className=" bg-gray-200">
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild suppressHydrationWarning>
+    <div>
+      <Sidebar collapsible="icon">
+        <SidebarHeader className="bg-sidebar">
+          <button
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            {state == "expanded" ? (
+              <Image
+                src="/reqoraLogo.png"
+                width={500}
+                height={500}
+                alt="logo"
+                className="w-50 h-25"
+              ></Image>
+            ) : (
+              ""
+            )}
+          </button>
+        </SidebarHeader>
+        <SidebarContent className="bg-sidebar text-text-primary">
+          <SidebarGroup>
+            <SidebarGroupLabel className=" font-bold">Main</SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenuItem>
                 <SidebarMenuButton
+                  tooltip="Dashboard"
+                  asChild
                   suppressHydrationWarning
-                  className="flex gap-2 "
+                  className="flex justify-start"
                 >
-                  <Icon icon="codicon:account" className="size-6" />
-                  <span className="font-semibold">Your account</span>
+                  <Button
+                    className="bg-transparent font-bold"
+                    variant={"secondary"}
+                  >
+                    <Icon
+                      icon="material-symbols-light:home-rounded"
+                      className="size-6!"
+                    />
+                    Dashboard
+                  </Button>
                 </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <LogoutDialog />
-                <Button
-                  onClick={() => {
-                    router.push("/profile");
-                  }}
-                  variant={"secondary"}
-                  className="min-w-full flex gap-2 justify-start cursor-pointer hover:bg-gray-200"
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+
+          <SidebarGroup>
+            <SidebarGroupLabel className="  font-bold">
+              Management
+            </SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild suppressHydrationWarning>
+                    <SidebarMenuButton
+                      tooltip="Resources"
+                      suppressHydrationWarning
+                      className="flex justify-start  font-bold"
+                    >
+                      <Icon
+                        icon="grommet-icons:resources"
+                        className="size-5!"
+                      />
+                      Resources
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem
+                      onClick={() => {
+                        router.push("/resources");
+                      }}
+                    >
+                      <Icon icon="material-symbols:grid-view-outline-rounded" />{" "}
+                      View Resources
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      onClick={() => {
+                        router.push("/admin/resources/add");
+                      }}
+                    >
+                      <Icon icon="hugeicons:resources-add" />
+                      Add Resources
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </SidebarMenuItem>
+
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild suppressHydrationWarning>
+                    <SidebarMenuButton
+                      tooltip="Employees"
+                      suppressHydrationWarning
+                      className="  font-bold"
+                    >
+                      <Icon icon="ic:baseline-people" className="size-5! " />
+                      Employees
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem
+                      onClick={() => {
+                        router.push("/admin/employees");
+                      }}
+                    >
+                      <Icon icon="material-symbols:view-day-rounded" /> View
+                      Employees
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      onClick={() => {
+                        router.push("/admin/invite");
+                      }}
+                    >
+                      <Icon icon="mdi:invite" />
+                      Invite Employees
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Icon icon="carbon:user-role" /> Manage Roles
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Icon icon="mingcute:user-remove-fill" />
+                      Remove Employees
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel className="  font-bold">
+              System
+            </SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  tooltip="Settings"
+                  suppressHydrationWarning
+                  className="  font-bold"
                 >
-                  <Icon icon="tabler:edit-filled" className="size-6" />
-                  <span>My profile</span>{" "}
-                </Button>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
-    </Sidebar>
+                  <Icon icon="material-symbols:settings" className="size-5!" />
+                  Settings
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  tooltip="Logs"
+                  suppressHydrationWarning
+                  className="  font-bold"
+                >
+                  <Icon icon="tabler:logs" className="size-5!" />
+                  <span>Logs</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter className=" bg-sidebar-border">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild suppressHydrationWarning>
+                  <SidebarMenuButton
+                    tooltip="Account"
+                    suppressHydrationWarning
+                    className="flex gap-2 "
+                  >
+                    <Icon icon="codicon:account" className="size-6" />
+                    <span className="font-semibold">Your account</span>
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <LogoutDialog />
+                  <Button
+                    onClick={() => {
+                      router.push("/profile");
+                    }}
+                    variant={"secondary"}
+                    className="min-w-full flex gap-2 justify-start cursor-pointer"
+                  >
+                    <Icon icon="tabler:edit-filled" className="size-6" />
+                    <span>My profile</span>
+                  </Button>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+    </div>
   );
 };
 
