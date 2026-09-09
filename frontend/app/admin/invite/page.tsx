1f4e21f5d9e309de1d0c083e5c0096233712add4
samantasubedi <samantasubedi01@gmail.com>
2026-09-09 10:42:42 +0545

    improving tab ui

--- frontend/app/admin/invite/page.tsx
+++ frontend/app/admin/invite/page.tsx
@@ -1,38 +1,28 @@
 "use client";
 import EmailInviteForm from "@/app/admin/components/EmailInviteForm";
 import InviteCodeGenerator from "@/app/admin/components/InviteCodeGenerator";
-
-import { Icon } from "@iconify/react";
-import React, { useState } from "react";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 const Page = () => {
-  const [emailForm, setEmailForm] = useState(true);
-
   return (
-    <div>
-      <div className="flex justify-center mt-5">
-        <div className="flex gap-10 w-[30%] border border-border justify-evenly px-5 bg-card shadow-sm shadow-border rounded-2xl p-2 ">
-          <button
-            onClick={() => {
-              setEmailForm(true);
-            }}
-            className={`flex items-center gap-3 p-2 rounded-xl font-semibold transition-colors duration-200 cursor-pointer ${emailForm ? "bg-primary text-primary-foreground" : " hover:text-card-foreground"}`}
-          >
-            <Icon icon="ic:outline-email" className="text-2xl" />
-            <span>Email Invite</span>
-          </button>
-          <button
-            onClick={() => {
-              setEmailForm(false);
-            }}
-            className={`flex items-center gap-3 p-2 rounded-xl font-semibold transition-colors duration-200 cursor-pointer ${!emailForm ? "bg-primary text-primary-foreground" : " hover:text-card-foreground"}`}
-          >
-            <Icon icon="pajamas:code" className="text-2xl" />
-            <span>Invite Code</span>
-          </button>
+    <div className="p-4">
+      <Tabs defaultValue="emailInvite">
+        <div className="w-full flex justify-center">
+          <TabsList className="w-[30%] h-12!">
+            <TabsTrigger className="font-bold" value="emailInvite">
+              Email Invite
+            </TabsTrigger>
+            <TabsTrigger className="font-bold" value="codeInvite">
+              Code Invite
+            </TabsTrigger>
+          </TabsList>
         </div>
-      </div>
-      {emailForm ? <EmailInviteForm /> : <InviteCodeGenerator />}
+        <TabsContent value="emailInvite">
+          <EmailInviteForm />
+        </TabsContent>
+        <TabsContent value="codeInvite">
+          <InviteCodeGenerator />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
